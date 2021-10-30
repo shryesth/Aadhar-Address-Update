f279c242a6ad4d3fb2ced2ac1bde842d7ca6166f
WaterHashira <lakshay6632@gmail.com>
2021-10-30 17:29:05 +0530

    Home Screen IP address checker

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LocationScreen(),
+      home: const HomeScreen(),
     );
   }
 }
